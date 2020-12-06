efd0b0d10558d1617669c2815c06e58946bd94b1
Nuria <nubango@ucm.es>
2020-12-06 02:23:05 +0100

    Clase JSONReader tiene estructura

--- OffTheLine/offTheLineLogic/src/main/java/es/ucm/gdv/offtheline/JSONReader.java
+++ OffTheLine/offTheLineLogic/src/main/java/es/ucm/gdv/offtheline/JSONReader.java
@@ -9,13 +9,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class JSONReader {
+
+    JSONArray jsonArray = null;
+    JSONObject level = null;
+
     public JSONReader(String path){
         JSONParser jsonParser = new JSONParser();
 
         Reader jsonReader = null;
-        JSONArray jsonArray = null;
         try {
             jsonReader = new FileReader(path);
         } catch (FileNotFoundException e) {
@@ -31,32 +36,65 @@ public class JSONReader {
 
         for (Object o : jsonArray)
         {
-            JSONObject level = (JSONObject) o;
-
-            String name = (String) level.get("name");
-            System.out.println("NOMBRE: " + name);
-
-            JSONArray paths = (JSONArray) level.get("paths");
-            System.out.println("CAMINOS: ");
-            for (Object c : paths)
-            {
-                System.out.println(c+"");
-            }
-
-            JSONArray items = (JSONArray) level.get("items");
-            System.out.println("MONEDAS: ");
-            for (Object c : items)
-            {
-                System.out.println(c+"");
-            }
-            /*
-            JSONArray enemies = (JSONArray) level.get("enemies");
-            System.out.println("ENEMIGOS: ");
-            for (Object c : enemies)
-            {
-                System.out.println(c+"");
-            }
-            */
+            level = (JSONObject) o;
+            // TESTEO PARA SABER QUE NIVEL ES
+            System.out.println("QUE NIVEL ES?");
+            if(this.getName().equals("THE BOX"))
+                System.out.println("ES EL NIVEL 1");
         }
+
     }
+
+    String getName(){
+        String name = (String) level.get("name");
+        System.out.println("NOMBRE: " + name);
+        return name;
+    }
+
+    ArrayList<Path> getPaths(){
+        JSONArray path = (JSONArray) level.get("path");
+        Iterator<Path> itr = path.listIterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+        /* EQUIVALENTE AL ITERADOR
+        for (Object p : path)
+        {
+            System.out.println(p+"");
+        }
+         */
+        return path;
+    }
+
+    ArrayList<Item> getItems(){
+        JSONArray items = (JSONArray) level.get("items");
+        Iterator<Item> itr = items.listIterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+        /* EQUIVALENTE AL ITERADOR
+        for (Object i : items)
+        {
+            System.out.println(i+"");
+        }
+         */
+        return items;
+    }
+
+    ArrayList<Enemy> getEnemies(){
+        JSONArray enemies = (JSONArray) level.get("enemies");
+        Iterator<Item> itr = enemies.listIterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+        /* EQUIVALENTE AL ITERADOR
+        for (Object e : enemies)
+        {
+            System.out.println(e+"");
+        }
+         */
+        return enemies;
+    }
+
+
 }
